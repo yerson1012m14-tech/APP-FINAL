dfb6344bdc4aa2d2ed79e4a679e8faa44265e588
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-20 09:28:50 -0500

    Update AppDelegate.m

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -1,37 +1,16 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
-#import "WebVC.h"
 
 @implementation AppDelegate
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    UIColor *acento = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
-    
-    UINavigationBarAppearance *ap = [[UINavigationBarAppearance alloc] init];
-    [ap configureWithOpaqueBackground];
-    ap.backgroundColor = [UIColor blackColor];
-    ap.shadowColor = [UIColor colorWithWhite:0.25 alpha:1.0];
-    ap.titleTextAttributes = @{
-        NSForegroundColorAttributeName: acento,
-        NSFontAttributeName: [UIFont fontWithName:@"Menlo-Bold" size:17]
-    };
-    [[UINavigationBar appearance] setStandardAppearance:ap];
-    [[UINavigationBar appearance] setScrollEdgeAppearance:ap];
-    [[UINavigationBar appearance] setTintColor:acento];
-    
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    [self mostrarRaiz];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(mostrarRaiz) name:@"KEY_ACTIVADA" object:nil];
+    BOOL act = [[NSUserDefaults standardUserDefaults] boolForKey:@"activado"];
+    UIViewController *root = act ? (UIViewController *)[MainVC new] : (UIViewController *)[KeyVC new];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:root];
+    nav.navigationBar.barStyle = UIBarStyleBlack;
+    nav.navigationBar.tintColor = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
+    self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     return YES;
 }
-
-- (void)mostrarRaiz {
-    BOOL activado = [[NSUserDefaults standardUserDefaults] boolForKey:@"activado"];
-    UIViewController *root = activado ? (UIViewController *)[WebVC new] : (UIViewController *)[KeyVC new];
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:root];
-    nav.navigationBarHidden = YES;
-    self.window.rootViewController = nav;
-}
-
 @end
