fab142c1c0cbf5b8d4de29ebed249a4b883205c9
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-20 09:56:58 -0500

    Update AppDelegate.m

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -2,15 +2,20 @@
 #import "ViewController.h"
 
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
     BOOL act = [[NSUserDefaults standardUserDefaults] boolForKey:@"activado"];
     UIViewController *root = act ? (UIViewController *)[MainVC new] : (UIViewController *)[KeyVC new];
+    
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:root];
     nav.navigationBar.barStyle = UIBarStyleBlack;
     nav.navigationBar.tintColor = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
+    
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     return YES;
 }
+
 @end
